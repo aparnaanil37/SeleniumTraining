c0c3f9e4307d94dd7e5ee9f2737aedf3d292bbb8
hp <hp@192.168.1.7>
2020-04-21 00:46:06 +0530

    first commit

--- sampleMaven/src/main/java/sampleMaven/HelloWorld.java
+++ sampleMaven/src/main/java/sampleMaven/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloooo Maven");
+		System.out.println("first commit to Git");
 	}
 
 }
